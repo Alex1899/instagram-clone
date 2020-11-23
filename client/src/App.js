fa75248c857b72f335b0c817d0947db4168e28d7
alex1899 <kvashilavaaleko@gmail.com>
2020-11-23 22:41:42 +0000

    Added Header.js at the top in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,48 @@
-import React, { useEffect } from 'react';
-import Home from './components/Home';
-import Header from './components/Header';
-import Profile from './components/Profile';
-import {BrowserRouter as Router, Switch, Route, useHistory} from 'react-router-dom'
-import Login from './components/auth/Login';
-import { useStateValue } from './context/StateProvider';
-import Register from './components/auth/Register';
-import Settings from './components/settings/Settings';
-import Footer from './components/Footer';
+import React, { useEffect } from "react";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
+import Login from "./components/auth/Login";
+import { useStateValue } from "./context/StateProvider";
+import Register from "./components/auth/Register";
+import Settings from "./components/settings/Settings";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
-          <Route exact path='/'>
-            <Header />
+          <Route exact path="/">
             <Home />
             <Footer />
           </Route>
-          <Route path='/login'>
+          <Route path="/login">
             <Login />
             <Footer />
           </Route>
-          <Route path='/register'>
+          <Route path="/register">
             <Register />
             <Footer />
           </Route>
-          <Route path='/profile'>
-            <Header />
+          <Route path="/profile">
             <Profile />
             <Footer />
           </Route>
-          <Route path='/account-edit'>
-            <Header />
+          <Route path="/account-edit">
             <Settings />
             <Footer />
           </Route>
         </Switch>
       </div>
     </Router>
-   
-
-   
   );
 }
 
